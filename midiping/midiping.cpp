393d3ffaeec6b2e40ee66f80a21a17c5ea288d82
Phil Burk <philburk@mobileer.com>
2022-07-11 18:34:22 -0700

    add AOSP header

--- midiping/midiping.cpp
+++ midiping/midiping.cpp
@@ -1,13 +1,23 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Measure ping time  for UDP or TCP packets across the network.
  *
- * Run the server using:
- *
- *       midiping
- *
- *   Note the port number. Determine the IP address for the server computer and enter:
- *
- *       midiping  {ipaddress}:{port}
+ * See README.md for instructions.
  *
  * Created by Phil Burk on 7/7/22.
  */
